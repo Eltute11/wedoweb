2848b85938f71719ac81c99e6e32761a66cb1eb4
Matias <arausmatias@gmail.com>
2021-01-22 21:36:54 -0300

    Buscador funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,27 @@ loadMore.addEventListener('click', function(){
 });
 
 
+function searchImage() {
+  // Declare variables
+  var input, filter, main_gallery, li, a, i, txtValue;
+  input = document.querySelector('#search');
+  filter = input.value.toUpperCase();
+  main_gallery = document.querySelector(".main_gallery");
+  item_gallery = main_gallery.querySelectorAll('.item_gallery');
+
+  console.log(item_gallery);
+
+  for (i = 0; i < item_gallery.length; i++) {
+    hashtag = item_gallery[i].querySelectorAll(".hashtag")[0];
+    txtValue = hashtag.textContent || hashtag.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      item_gallery[i].style.display = "";
+    } else {
+      item_gallery[i].style.display = "none";
+    }
+  }
+}
+
 // Activamos el Lightbox
 lightbox.option({
   'resizeDuration': 200,
